3ca81b4349db29147753c7b57d83370441060a1f
kr328 <kr328app@outlook.com>
2022-04-21 15:29:19 +0800

    Chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 subprojects {
     group = "com.github.kr328.krestful"
-    version = "2.0"
+    version = "2.1"
 
     plugins.withId("maven-publish") {
         extensions.configure<PublishingExtension> {
